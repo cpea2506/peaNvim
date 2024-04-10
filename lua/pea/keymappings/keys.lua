edbc49b2af392e6d9a738dc88a344bf188626569
CPea <cpea2506@gmail.com>
2024-04-11 01:17:12 +0700

    Remove default move line keys

--- lua/pea/keymappings/keys.lua
+++ lua/pea/keymappings/keys.lua
@@ -9,22 +9,12 @@ local keymapping = {
 		["<C-j>"] = "<C-w>j",
 		["<C-k>"] = "<C-w>k",
 		["<C-l>"] = "<C-w>l",
-
-		-- Move current line / block.
-		["<M-j>"] = ":m .+1<cr>==",
-		["<M-k>"] = ":m .-2<cr>==",
 	},
 	i = {},
 	v = {
 		-- Better indenting
 		["<"] = "<gv",
 		[">"] = ">gv",
-	},
-
-	x = {
-		-- Move current line / block.
-		["<M-j>"] = ":m '>+1<cr>gv-gv",
-		["<M-k>"] = ":m '<-2<cr>gv-gv",
 	},
 	c = {
 		-- Navigate tab completion with <c-j> and <c-k>.
