d49b006fea33226401602744decb5a18017f5690
CPea <cpea2506@gmail.com>
2024-04-11 01:39:07 +0700

    Add rainbow delimeter plugin

--- lua/pea/plugins/treesitter.lua
+++ lua/pea/plugins/treesitter.lua
@@ -1,47 +1,51 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	cmd = {
-		"TSInstall",
-		"TSUninstall",
-		"TSUpdate",
-		"TSUpdateSync",
-		"TSInstallInfo",
-		"TSInstallSync",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		cmd = {
+			"TSInstall",
+			"TSUninstall",
+			"TSUpdate",
+			"TSUpdateSync",
+			"TSInstallInfo",
+			"TSInstallSync",
+		},
+		keys = {
+			{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
+		},
+		event = "VeryLazy",
+		dependencies = "HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = false,
+				ensure_installed = {
+					"bash",
+					"comment",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"regex",
+					"rust",
+					"toml",
+					"vim",
+					"yaml",
+				},
+				autotag = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+				rainbow = {
+					enable = true,
+				},
+			})
+		end,
 	},
-	keys = {
-		{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		lazy = true,
 	},
-	event = "VeryLazy",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			sync_install = true,
-			auto_install = false,
-			ensure_installed = {
-				"bash",
-				"comment",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"regex",
-				"rust",
-				"toml",
-				"vim",
-				"yaml",
-			},
-			autotag = {
-				enable = true,
-			},
-			highlight = {
-				enable = true,
-			},
-			rainbow = {
-				enable = true,
-			},
-			playground = {
-				enable = true,
-			},
-		})
-	end,
 }
