d4af87528b13517eb3af4ebc541042a345415588
CPea <cpea2506@gmail.com>
2024-04-11 23:00:15 +0700

    Replace `none-ls` with separated formatter and linter

---
+++ lua/pea/plugins/lsp/lints.lua
@@ -0,0 +1,70 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+			linters_by_ft = {
+				lua = { "selene" },
+			},
+		},
+		config = function(_, opts)
+			local M = {}
+
+			local lint = require("lint")
+
+			lint.linters_by_ft = opts.linters_by_ft
+
+			function M.debounce(ms, fn)
+				local timer = vim.uv.new_timer()
+
+				return function(...)
+					local argv = { ... }
+
+					if timer then
+						timer:start(ms, 0, function()
+							timer:stop()
+							vim.schedule_wrap(fn)(unpack(argv))
+						end)
+					end
+				end
+			end
+
+			function M.lint()
+				local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+				names = vim.list_extend({}, names)
+
+				if #names == 0 then
+					vim.list_extend(names, lint.linters_by_ft["_"] or {})
+				end
+
+				vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+				-- Filter out linters that don't exist or don't match the condition.
+				local ctx = {
+					filename = vim.api.nvim_buf_get_name(0),
+				}
+				ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+
+				names = vim.tbl_filter(function(name)
+					local linter = lint.linters[name]
+
+					if not linter then
+						vim.notify_once("Linter not found: " .. name, vim.log.levels.WARN, { title = "nvim-lint" })
+					end
+
+					return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+				end, names)
+
+				if #names > 0 then
+					lint.try_lint(names)
+				end
+			end
+
+			vim.api.nvim_create_autocmd(opts.events, {
+				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+				callback = M.debounce(100, M.lint),
+			})
+		end,
+	},
+}
