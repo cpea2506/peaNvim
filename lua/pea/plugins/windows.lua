6d22c28f256c5f08dcd00dbc63e6a847d7815934
CPea <cpea2506@gmail.com>
2024-04-14 04:15:24 +0700

    Load `windows` plugin on `BufRead` event

--- lua/pea/plugins/windows.lua
+++ lua/pea/plugins/windows.lua
@@ -1,22 +1,15 @@
 return {
 	{
 		"anuvyklack/windows.nvim",
-		keys = {
-			{ "<C-w>a", "<cmd>WindowsMaximize<cr>", desc = "Windows Maximize" },
-			{ "<C-w>=", "<cmd>WindowsEqualize<cr>", desc = "Windows Equalize" },
-		},
-		config = true,
 		dependencies = {
 			"anuvyklack/middleclass",
 			"anuvyklack/animation.nvim",
 		},
-	},
-	{
-		"anuvyklack/middleclass",
-		lazy = true,
-	},
-	{
-		"anuvyklack/animation.nvim",
-		lazy = true,
+		keys = {
+			{ "<C-w>a", "<cmd>WindowsMaximize<cr>", desc = "Windows Maximize" },
+			{ "<C-w>=", "<cmd>WindowsEqualize<cr>", desc = "Windows Equalize" },
+		},
+		event = "BufRead",
+		config = true,
 	},
 }
