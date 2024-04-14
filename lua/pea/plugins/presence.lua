a697b7bb9163486db63fe8aa4e7886f16c6bce61
CPea <cpea2506@gmail.com>
2024-04-14 12:24:06 +0700

    Add `presence` plugin to display activity to Discord

---
+++ lua/pea/plugins/presence.lua
@@ -0,0 +1,14 @@
+return {
+	"jiriks74/presence.nvim",
+	event = "UIEnter",
+	opts = {
+		neovim_image_text = "Neovim",
+		main_image = "file",
+		log_level = nil,
+		client_id = "914799712794705961",
+		show_time = false,
+		workspace_text = function()
+			return "Crababarian 🦀"
+		end,
+	},
+}
