e1a592915e5e0b4fd0e53e4f7675c12a6d9ccc33
CPea <cpea2506@gmail.com>
2024-04-14 12:00:31 +0700

    Add `nvim-treesitter-context` plugin

--- lua/pea/plugins/treesitter.lua
+++ lua/pea/plugins/treesitter.lua
@@ -1,47 +1,57 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	cmd = {
-		"TSInstall",
-		"TSUninstall",
-		"TSUpdate",
-		"TSUpdateSync",
-		"TSInstallInfo",
-		"TSInstallSync",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		cmd = {
+			"TSInstall",
+			"TSUninstall",
+			"TSUpdate",
+			"TSUpdateSync",
+			"TSInstallInfo",
+			"TSInstallSync",
+		},
+		keys = {
+			{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
+		},
+		event = { "BufRead", "VeryLazy" },
+		dependencies = "HiPhish/rainbow-delimiters.nvim",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = true,
+				auto_install = false,
+				ensure_installed = {
+					"bash",
+					"comment",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"regex",
+					"rust",
+					"c_sharp",
+					"toml",
+					"vim",
+					"yaml",
+					"vimdoc",
+				},
+				autotag = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+				},
+				rainbow = {
+					enable = true,
+				},
+			})
+		end,
 	},
-	keys = {
-		{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "BufRead",
+		opts = {
+			mode = "cursor",
+			max_lines = 3,
+		},
 	},
-	event = "VeryLazy",
-	dependencies = "HiPhish/rainbow-delimiters.nvim",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			sync_install = true,
-			auto_install = false,
-			ensure_installed = {
-				"bash",
-				"comment",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"regex",
-				"rust",
-				"c_sharp",
-				"toml",
-				"vim",
-				"yaml",
-				"vimdoc",
-			},
-			autotag = {
-				enable = true,
-			},
-			highlight = {
-				enable = true,
-			},
-			rainbow = {
-				enable = true,
-			},
-		})
-	end,
 }
