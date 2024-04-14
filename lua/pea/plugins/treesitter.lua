e43ad6f80240169afe12649335bccb57d43216da
CPea <cpea2506@gmail.com>
2024-04-14 10:59:27 +0700

    Remove `rainbow-delimiters.nvim` as independent plugin

--- lua/pea/plugins/treesitter.lua
+++ lua/pea/plugins/treesitter.lua
@@ -1,52 +1,46 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		cmd = {
-			"TSInstall",
-			"TSUninstall",
-			"TSUpdate",
-			"TSUpdateSync",
-			"TSInstallInfo",
-			"TSInstallSync",
-		},
-		keys = {
-			{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
-		},
-		event = "VeryLazy",
-		dependencies = "HiPhish/rainbow-delimiters.nvim",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				sync_install = true,
-				auto_install = false,
-				ensure_installed = {
-					"bash",
-					"comment",
-					"json",
-					"lua",
-					"markdown",
-					"markdown_inline",
-					"regex",
-					"rust",
-					"toml",
-					"vim",
-					"yaml",
-					"vimdoc",
-				},
-				autotag = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
-				},
-				rainbow = {
-					enable = true,
-				},
-			})
-		end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	cmd = {
+		"TSInstall",
+		"TSUninstall",
+		"TSUpdate",
+		"TSUpdateSync",
+		"TSInstallInfo",
+		"TSInstallSync",
 	},
-	{
-		"HiPhish/rainbow-delimiters.nvim",
-		lazy = true,
+	keys = {
+		{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
 	},
+	event = "VeryLazy",
+	dependencies = "HiPhish/rainbow-delimiters.nvim",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			sync_install = true,
+			auto_install = false,
+			ensure_installed = {
+				"bash",
+				"comment",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"rust",
+				"toml",
+				"vim",
+				"yaml",
+				"vimdoc",
+			},
+			autotag = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
+			rainbow = {
+				enable = true,
+			},
+		})
+	end,
 }
