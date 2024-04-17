75643ec3ae0d987c2fccf527b4f21d7dc0939c3b
CPea <cpea2506@gmail.com>
2024-04-18 03:00:57 +0700

    Add `csharpier` as csharp formatter

--- lua/pea/plugins/formatters.lua
+++ lua/pea/plugins/formatters.lua
@@ -5,6 +5,7 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
+			cs = { "csharpier" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
@@ -13,5 +14,18 @@ return {
 	},
 	init = function()
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+	config = function(_, opts)
+		local conform = require("conform")
+		local utils = require("conform.util")
+
+		opts.formatters = {
+			csharpier = {
+				cwd = utils.root_file({ ".csharpierrc" }),
+				require_cwd = true,
+			},
+		}
+
+		conform.setup(opts)
 	end,
 }
