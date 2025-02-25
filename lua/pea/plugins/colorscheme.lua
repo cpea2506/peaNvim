c13c2317832eb8cb37b12b0f8d24e0b53dcf75aa
CPea <cpea2506@gmail.com>
2025-02-25 22:08:51 +0700

    Customize some c# semantic highlight groups

--- lua/pea/plugins/colorscheme.lua
+++ lua/pea/plugins/colorscheme.lua
@@ -4,5 +4,14 @@ return {
 	priority = 1000,
 	opts = {
 		transparent = true,
+		themes = function(colors)
+			return {
+				["@lsp.type.event.cs"] = { fg = colors.yellow },
+				["@lsp.type.delegate.cs"] = { link = "@function" },
+				["@lsp.type.keyword.cs"] = { fg = colors.pink },
+				["@lsp.type.constant.cs"] = { link = "@constant" },
+				["@lsp.type.interface.cs"] = { link = "@type" },
+			}
+		end,
 	},
 }
