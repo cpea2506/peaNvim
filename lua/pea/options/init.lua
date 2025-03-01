9c5e75b40d71a588070dcc60c2a79666cc1bffb3
CPea <cpea2506@gmail.com>
2025-03-01 23:27:26 +0700

    Fix options cannot be set

--- lua/pea/options/init.lua
+++ lua/pea/options/init.lua
@@ -1,11 +1,11 @@
 local vim_options = require("lua.pea.options.vim_options")
 
 for option, value in pairs(vim_options) do
-	vim_options.opt[option] = value
+	vim.opt[option] = value
 end
 
 local global_options = require("lua.pea.options.global_options")
 
 for option, value in pairs(global_options) do
-	vim_options.g[option] = value
+	vim.g[option] = value
 end
