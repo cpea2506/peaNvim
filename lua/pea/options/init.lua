71658db77f766fd2dc1d48aa0386f3c238608b3e
CPea <cpea2506@gmail.com>
2025-03-03 10:01:12 +0700

    Fix options cannot be loaded outside of config location

--- lua/pea/options/init.lua
+++ lua/pea/options/init.lua
@@ -1,10 +1,10 @@
-local vim_options = require("lua.pea.options.vim_options")
+local vim_options = require("pea.options.vim_options")
 
 for option, value in pairs(vim_options) do
 	vim.opt[option] = value
 end
 
-local global_options = require("lua.pea.options.global_options")
+local global_options = require("pea.options.global_options")
 
 for option, value in pairs(global_options) do
 	vim.g[option] = value
