8903af439f847ed16756045d3a42a42b439e7c30
CPea <cpea2506@gmail.com>
2025-03-03 09:43:12 +0700

    Update treesitter config

--- lua/pea/plugins/treesitter.lua
+++ lua/pea/plugins/treesitter.lua
@@ -14,26 +14,29 @@ return {
 			{ "<leader>Tu", "<cmd>TSUpdateSync<cr>", desc = "Treesitter Update Sync" },
 		},
 		event = { "BufRead", "VeryLazy" },
-		dependencies = "HiPhish/rainbow-delimiters.nvim",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				sync_install = true,
-				auto_install = true,
-				autotag = {
-					enable = true,
-				},
-				highlight = {
-					enable = true,
-				},
-				rainbow = {
-					enable = true,
-				},
-			})
+		dependencies = {
+			"HiPhish/rainbow-delimiters.nvim",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		opts = {
+			sync_install = true,
+			auto_install = true,
+			autotag = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
+			rainbow = {
+				enable = true,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		event = "BufRead",
 		opts = {
 			mode = "cursor",
 			max_lines = 3,
