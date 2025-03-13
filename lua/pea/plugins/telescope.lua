4131bc780421e7e15fb8ecb845b2c99c43549cc0
CPea <cpea2506@gmail.com>
2025-03-13 09:56:51 +0700

    Update fzf-native build command

--- lua/pea/plugins/telescope.lua
+++ lua/pea/plugins/telescope.lua
@@ -133,6 +133,6 @@ return {
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "make",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 	},
 }
