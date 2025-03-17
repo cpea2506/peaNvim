1abbcc9930a35a203cd1baf6e89dd8695dcffc58
CPea <cpea2506@gmail.com>
2025-03-18 00:44:04 +0700

    Format on save on modification only

--- lua/pea/plugins/formatters.lua
+++ lua/pea/plugins/formatters.lua
@@ -2,6 +2,9 @@ return {
 	"stevearc/conform.nvim",
 	event = "BufWritePre",
 	cmd = "ConformInfo",
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -12,10 +15,6 @@ return {
 			javascript = { "prettier" },
 			typescript = { "prettier" },
 		},
-		format_on_save = {
-			async = true,
-			lsp_format = "fallback",
-		},
 		formatters = {
 			csharpier = {
 				cwd = function(_, ctx)
@@ -24,8 +23,38 @@ return {
 				require_cwd = true,
 			},
 		},
+		format_on_save = function(bufnr)
+			local ignore_filetypes = { "lua" }
+
+			if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+				return { lsp_format = "fallback" }
+			end
+
+			-- SAFETY: gitsigns is always loaded before conform.
+			local gitsigns = require("gitsigns")
+			local hunks = gitsigns.get_hunks(bufnr)
+
+			if hunks == nil then
+				return
+			end
+
+			local conform = require("conform")
+
+			for i = #hunks, 1, -1 do
+				local hunk = hunks[i]
+
+				if hunk ~= nil and hunk.type ~= "delete" then
+					local start = hunk.added.start
+					local last = start + hunk.added.count
+					local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+					local range = {
+						start = { start, 0 },
+						["end"] = { last - 1, last_hunk_line:len() },
+					}
+
+					conform.format({ range = range, lsp_format = "fallback" })
+				end
+			end
+		end,
 	},
-	init = function()
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
 }
