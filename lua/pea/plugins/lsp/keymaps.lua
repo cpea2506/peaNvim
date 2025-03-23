a39de3c111fdfd21104b4aea6fef7682234bf48e
CPea <cpea2506@gmail.com>
2025-03-23 23:32:35 +0700

    Create lsp keymaps table only on attach

--- lua/pea/plugins/lsp/keymaps.lua
+++ lua/pea/plugins/lsp/keymaps.lua
@@ -1,16 +1,16 @@
 local M = {}
 
-local keymaps = {
-	{ "n", "gd", vim.lsp.buf.definition, { desc = "Definition" } },
-	{ "n", "gD", vim.lsp.buf.type_definition, { desc = "Type Definition" } },
-	{ "n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" } },
-	{ "n", "gn", vim.lsp.buf.rename, { desc = "Rename" } },
-	{ "n", "ga", vim.lsp.buf.code_action, { desc = "Code Action" } },
-	{ "n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true } },
-	{ "n", "gi", vim.lsp.buf.implementation, { desc = "Implementation" } },
-}
-
 function M.on_attach(_, bufnr)
+	local keymaps = {
+		{ "n", "gd", vim.lsp.buf.definition, { desc = "Definition" } },
+		{ "n", "gD", vim.lsp.buf.type_definition, { desc = "Type Definition" } },
+		{ "n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" } },
+		{ "n", "gn", vim.lsp.buf.rename, { desc = "Rename" } },
+		{ "n", "ga", vim.lsp.buf.code_action, { desc = "Code Action" } },
+		{ "n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true } },
+		{ "n", "gi", vim.lsp.buf.implementation, { desc = "Implementation" } },
+	}
+
 	for _, key in pairs(keymaps) do
 		local opts = key[4] or {}
 		opts.silent = true
