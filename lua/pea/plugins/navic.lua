f6a696ad7fe00e6544ea40fc9865697cf29d570f
CPea <cpea2506@gmail.com>
2025-03-26 11:07:36 +0700

    Refactor winbar

--- lua/pea/plugins/navic.lua
+++ lua/pea/plugins/navic.lua
@@ -1,106 +1,74 @@
-local icons = require("pea.config.ui.icons")
-
-local function isempty(s)
-	return s == nil or s == ""
-end
-
-local function get_filename()
-	local filename = vim.fn.expand("%:t")
-	local extension = vim.fn.expand("%:e")
-
-	if not isempty(filename) then
-		local devicons = require("nvim-web-devicons")
-		local file_icon, hl_group = devicons.get_icon(filename, extension, { default = true })
-
-		if isempty(file_icon) then
-			file_icon = icons.kind.File
-		end
-
-		local text_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-		vim.api.nvim_set_hl(0, "Winbar", { fg = text_hl.foreground })
-
-		return " " .. "%#" .. hl_group .. "#" .. file_icon .. "%*" .. " " .. "%#Winbar#" .. filename .. "%*"
-	end
-end
-
-local function get_location()
-	local nvim_navic = require("nvim-navic")
-
-	local location = nvim_navic.get_location()
-
-	if not nvim_navic.is_available() or location == "error" then
-		return ""
-	end
-
-	if not isempty(location) then
-		return "%#NavicSeparator#" .. icons.ui.ChevronRight .. "%* " .. location
-	else
-		return ""
-	end
-end
-
-local function set_winbar()
-	local value = get_filename()
-	local location_added = false
-
-	if not isempty(value) then
-		local location_value = get_location()
-
-		value = value .. " " .. location_value
-
-		if not isempty(location_value) then
-			location_added = true
-		end
-	end
-
-	if not isempty(value) and vim.api.nvim_get_option_value("mod", { buf = 0 }) then
-		local mod = "%#LspCodeLens#" .. icons.ui.Circle .. "%*"
-
-		if location_added then
-			value = value .. " " .. mod
-		else
-			value = value .. mod
-		end
-	end
-
-	local num_tabs = #vim.api.nvim_list_tabpages()
-
-	if num_tabs > 1 and not isempty(value) then
-		local tabpage_number = tostring(vim.api.nvim_tabpage_get_number(0))
-
-		value = value .. "%=" .. tabpage_number .. "/" .. tostring(num_tabs)
-	end
-
-	vim.api.nvim_set_option_value("winbar", value, { scope = "local" })
-end
-
 return {
 	"smiteshp/nvim-navic",
 	event = "LazyFile",
-	opts = {
-		icons = vim.tbl_map(function(icon)
-			return icon .. " "
-		end, icons.kind),
-		lsp = {
-			auto_attach = true,
-		},
-		depth_limit = 0,
-		depth_limit_indicator = "..",
-		separator = " " .. icons.ui.ChevronRight .. " ",
-		highlight = true,
-		exclude_filetypes = {
-			"help",
-			"lazy",
-			"NvimTree",
-			"toggleterm",
-			"noice",
-			"",
-		},
-	},
-	config = function(_, opts)
-		require("nvim-navic").setup(opts)
+	opts = function()
+		local icons = require("pea.config.ui.icons")
 
-		local winbar_group = vim.api.nvim_create_augroup("navic-winbar", { clear = true })
+		return {
+			icons = vim.tbl_map(function(icon)
+				return icon .. " "
+			end, icons.kind),
+			lsp = {
+				auto_attach = true,
+			},
+			depth_limit = 0,
+			depth_limit_indicator = "..",
+			separator = " " .. icons.ui.ChevronRight .. " ",
+			highlight = true,
+			exclude_filetypes = {
+				"help",
+				"lazy",
+				"NvimTree",
+				"toggleterm",
+				"noice",
+				"",
+			},
+		}
+	end,
+	config = function(_, opts)
+		local navic = require("nvim-navic")
+		local icons = require("pea.config.ui.icons")
+
+		navic.setup(opts)
+
+		local function is_empty(s)
+			return s == nil or s == ""
+		end
+
+		local function get_filename()
+			local filename = vim.fn.expand("%:t")
+
+			if not is_empty(filename) then
+				local extension = vim.fn.expand("%:e")
+				local devicons = require("nvim-web-devicons")
+				local fileicon, hlgroup = devicons.get_icon(filename, extension, { default = true })
+
+				if is_empty(fileicon) then
+					fileicon = icons.kind.File
+				end
+
+				local text_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+				vim.api.nvim_set_hl(0, "Winbar", { fg = text_hl.foreground })
+
+				return " " .. "%#" .. hlgroup .. "#" .. fileicon .. "%*" .. " " .. "%#Winbar#" .. filename .. "%*"
+			end
+		end
+
+		local function get_location()
+			if not navic.is_available() then
+				return ""
+			end
+
+			local location = navic.get_location()
+
+			if not is_empty(location) and location ~= "error" then
+				return "%#NavicSeparator#" .. icons.ui.ChevronRight .. "%* " .. location
+			end
+
+			return ""
+		end
+
+		local augroup = vim.api.nvim_create_augroup("navic-winbar", { clear = true })
 
 		vim.api.nvim_create_autocmd({
 			"CursorHoldI",
@@ -112,13 +80,35 @@ return {
 			"TabClosed",
 			"TabEnter",
 		}, {
-			group = winbar_group,
+			group = augroup,
 			callback = function(args)
 				if vim.tbl_contains(opts.exclude_filetypes, vim.bo[args.buf].filetype) then
 					return
 				end
 
-				set_winbar()
+				local winbar = get_filename()
+				local location_added = false
+
+				if not is_empty(winbar) then
+					local location = get_location()
+					winbar = winbar .. " " .. location
+
+					if not is_empty(location) then
+						location_added = true
+					end
+				end
+
+				if not is_empty(winbar) and vim.api.nvim_get_option_value("mod", { buf = 0 }) then
+					local modified = "%#LspCodeLens#" .. icons.ui.Circle .. "%*"
+
+					if location_added then
+						winbar = winbar .. " " .. modified
+					else
+						winbar = winbar .. modified
+					end
+				end
+
+				vim.api.nvim_set_option_value("winbar", winbar, { scope = "local" })
 			end,
 		})
 	end,
