d15bcc7df85b8123f208e210a0e10a4dbdaa6864
CPea <cpea2506@gmail.com>
2025-03-28 00:56:39 +0700

    Set cursor shape for terminal mode

--- lua/pea/config/options.lua
+++ lua/pea/config/options.lua
@@ -43,7 +43,7 @@ local options = {
 		fileencoding = "utf-8",
 		clipboard = "unnamedplus",
 		fillchars = { eob = " " },
-		guicursor = "i-ci-ve:hor30",
+		guicursor = "i-ci-ve-t:hor30",
 		guifont = "SFMono Nerd Font:h13",
 		completeopt = { "menu", "menuone", "noselect" },
 	},
