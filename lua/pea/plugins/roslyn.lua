e24e116edd1ae0b2812de7803b5749ff5119a0e8
CPea <cpea2506@gmail.com>
2025-04-01 18:13:12 +0700

    Remove semantic token hacks from roslyn

--- lua/pea/plugins/roslyn.lua
+++ lua/pea/plugins/roslyn.lua
@@ -103,47 +103,6 @@ return {
 						apply_vs_text_edit(result._vs_textEdit)
 					end,
 				},
-				on_attach = function(client, _)
-					if client.is_hacked then
-						return
-					end
-
-					client.is_hacked = true
-
-					-- let the runtime know the server can do semanticTokens/full now
-					client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
-						semanticTokensProvider = {
-							full = true,
-						},
-					})
-
-					-- monkey patch the request proxy
-					local request_inner = client.request
-
-					function client:request(method, params, handler, req_bufnr)
-						if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-							return request_inner(self, method, params, handler)
-						end
-
-						local target_bufnr = vim.uri_to_bufnr(params.textDocument.uri)
-						local line_count = vim.api.nvim_buf_line_count(target_bufnr)
-						local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
-
-						return request_inner(self, "textDocument/semanticTokens/range", {
-							textDocument = params.textDocument,
-							range = {
-								["start"] = {
-									line = 0,
-									character = 0,
-								},
-								["end"] = {
-									line = line_count - 1,
-									character = string.len(last_line) - 1,
-								},
-							},
-						}, handler, req_bufnr)
-					end
-				end,
 			},
 			filewatching = "roslyn",
 		}
