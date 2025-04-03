67875c2f458e170bfa9267a3df116b4569bbd1b4
CPea <cpea2506@gmail.com>
2025-04-03 15:30:23 +0700

    Support code action on visual mode

--- lua/pea/plugins/lsp/keymaps.lua
+++ lua/pea/plugins/lsp/keymaps.lua
@@ -6,7 +6,7 @@ function M.on_attach(_, bufnr)
 		{ "n", "gD", vim.lsp.buf.type_definition, { desc = "Type Definition" } },
 		{ "n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" } },
 		{ "n", "gn", vim.lsp.buf.rename, { desc = "Rename" } },
-		{ "n", "ga", vim.lsp.buf.code_action, { desc = "Code Action" } },
+		{ { "n", "v" }, "ga", vim.lsp.buf.code_action, { desc = "Code Action" } },
 		{ "n", "gr", vim.lsp.buf.references, { desc = "References", nowait = true } },
 		{ "n", "gi", vim.lsp.buf.implementation, { desc = "Implementation" } },
 	}
