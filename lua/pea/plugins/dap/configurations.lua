375e49aa0d26c72c746fd8de0d85c6936473b5f9
CPea <cpea2506@gmail.com>
2025-05-25 23:28:55 +0700

    Replace `nvim-dap-ui` with `debugmaster`

--- lua/pea/plugins/dap/configurations.lua
+++ lua/pea/plugins/dap/configurations.lua
@@ -1,15 +1,4 @@
-local M = {}
-
-local vstuc_path = vim.env.HOME .. "/.vscode-insiders/extensions/visualstudiotoolsforunity.vstuc-1.1.1/bin/"
-
-local adapters = {
-	vstuc = {
-		type = "executable",
-		command = "dotnet",
-		args = { vstuc_path .. "UnityDebugAdapter.dll" },
-		name = "Attach to Unity",
-	},
-}
+local utils = require("pea.plugins.dap.utils")
 
 local configurations = {
 	cs = {
@@ -37,7 +26,7 @@ local configurations = {
 				end
 			end,
 			endPoint = function()
-				local system_obj = vim.system({ "dotnet", vstuc_path .. "UnityAttachProbe.dll" }, { text = true })
+				local system_obj = vim.system({ "dotnet", utils.vstuc_path .. "UnityAttachProbe.dll" }, { text = true })
 				local probe_result = system_obj:wait(2000).stdout
 
 				if probe_result == nil or #probe_result == 0 then
@@ -66,16 +55,4 @@ local configurations = {
 	},
 }
 
-M.setup = function()
-	local dap = require("dap")
-
-	for adapter, config in pairs(adapters) do
-		dap.adapters[adapter] = config
-	end
-
-	for filetype, config in pairs(configurations) do
-		dap.configurations[filetype] = config
-	end
-end
-
-return M
+return configurations
--- lua/pea/plugins/dap/init.lua
+++ lua/pea/plugins/dap/init.lua
@@ -1,114 +1,66 @@
 return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = {
+	{
 		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio",
+		config = function()
+			local dap = require("dap")
+			local adapters = require("pea.plugins.dap.adapters")
+			local configurations = require("pea.plugins.dap.configurations")
+
+			for adapter, config in pairs(adapters) do
+				dap.adapters[adapter] = config
+			end
+
+			for filetype, config in pairs(configurations) do
+				dap.configurations[filetype] = config
+			end
+
+			local icons = require("pea.ui.icons")
+
+			vim.fn.sign_define("DapBreakpoint", {
+				text = icons.ui.Circle,
+				texthl = "DapBreakpoint",
+				linehl = "Visual",
+				numhl = "DapBreakpoint",
+			})
+			vim.fn.sign_define("DapBreakpointCondition", {
+				text = icons.ui.CircleInfo,
+				texthl = "DapBreakpoint",
+				linehl = "Visual",
+				numhl = "DapBreakpoint",
+			})
+			vim.fn.sign_define("DapBreakpointRejected", {
+				text = icons.ui.CircleWarning,
+				texthl = "DapBreakpoint",
+				linehl = "Visual",
+				numhl = "DapBreakpoint",
+			})
+			vim.fn.sign_define("DapLogPoint", {
+				text = icons.ui.CircleInfo,
+				texthl = "DapLogPoint",
+				linehl = "Visual",
+				numhl = "DapLogPoint",
+			})
+			vim.fn.sign_define("DapStopped", {
+				text = icons.ui.CirclePlay,
+				texthl = "DapStopped",
+				linehl = "Visual",
+				numhl = "DapStopped",
+			})
+		end,
 	},
-	keys = {
-		{ "<F5>", "<cmd>DapContinue<cr>", desc = "Dap Continue" },
-		{ "<F10>", "<cmd>DapStepOver<cr>", desc = "Dap Step Over" },
-		{ "<F11>", "<cmd>DapStepInto<cr>", desc = "Dap Step Into" },
-		{ "<F12>", "<cmd>DapStepOut<cr>", desc = "Dap Step Out" },
-		{ "<leader>k", "<cmd>DapEval<cr>", desc = "Dap Eval", mode = { "n", "v" } },
-		{ "<leader>db", "<cmd>DapToggleBreakpoint<cr>", desc = "Dap Toggle Breakpoint" },
-		{ "<leader>dt", [[<cmd>lua require("dapui").toggle({ reset = true })<cr>]], desc = "Dap Toggle" },
+	{
+		"miroshQa/debugmaster.nvim",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		keys = "<leader>d",
+		config = function()
+			local debugmaster = require("debugmaster")
+
+			debugmaster.plugins.cursor_hl.enabled = true
+			debugmaster.plugins.ui_auto_toggle.enabled = true
+
+			vim.keymap.set({ "n", "v" }, "<leader>d", debugmaster.mode.toggle, { nowait = true })
+		end,
 	},
-	opts = function()
-		local icons = require("pea.ui.icons")
-
-		return {
-			controls = {
-				icons = {
-					play = icons.ui.Triangle,
-					terminate = icons.ui.Square,
-				},
-			},
-			floating = {
-				border = "rounded",
-			},
-			layouts = {
-				{
-					elements = {
-						{
-							id = "scopes",
-							size = 0.25,
-						},
-						{
-							id = "breakpoints",
-							size = 0.25,
-						},
-						{
-							id = "stacks",
-							size = 0.25,
-						},
-						{
-							id = "watches",
-							size = 0.25,
-						},
-					},
-					position = "right",
-					size = 40,
-				},
-				{
-					elements = {
-						{
-							id = "console",
-							size = 0.5,
-						},
-						{
-							id = "repl",
-							size = 0.5,
-						},
-					},
-					position = "bottom",
-					size = 10,
-				},
-			},
-		}
-	end,
-	config = function(_, opts)
-		local dap = require("dap")
-		local dapui = require("dapui")
-		local icons = require("pea.ui.icons")
-
-		dapui.setup(opts)
-
-		dap.listeners.before.attach.dapui_config = dapui.open
-		dap.listeners.before.launch.dapui_config = dapui.open
-		dap.listeners.before.event_terminated.dapui_config = dapui.close
-		dap.listeners.before.event_exited.dapui_config = dapui.close
-
-		vim.fn.sign_define("DapBreakpoint", {
-			text = icons.ui.Circle,
-			texthl = "DapBreakpoint",
-			linehl = "Visual",
-			numhl = "DapBreakpoint",
-		})
-		vim.fn.sign_define("DapBreakpointCondition", {
-			text = icons.ui.CircleInfo,
-			texthl = "DapBreakpoint",
-			linehl = "Visual",
-			numhl = "DapBreakpoint",
-		})
-		vim.fn.sign_define("DapBreakpointRejected", {
-			text = icons.ui.CircleWarning,
-			texthl = "DapBreakpoint",
-			linehl = "Visual",
-			numhl = "DapBreakpoint",
-		})
-		vim.fn.sign_define("DapLogPoint", {
-			text = icons.ui.CircleInfo,
-			texthl = "DapLogPoint",
-			linehl = "Visual",
-			numhl = "DapLogPoint",
-		})
-		vim.fn.sign_define("DapStopped", {
-			text = icons.ui.CirclePlay,
-			texthl = "DapStopped",
-			linehl = "Visual",
-			numhl = "DapStopped",
-		})
-
-		require("pea.plugins.dap.config").setup()
-	end,
 }
