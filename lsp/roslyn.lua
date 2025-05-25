10926563516c7c4d28b9f818922d7e0e70d8ae2c
CPea <cpea2506@gmail.com>
2025-05-25 13:30:38 +0700

    Inline vs_textEdit handler from Roslyn

--- lsp/roslyn.lua
+++ lsp/roslyn.lua
@@ -1,42 +1,3 @@
-local function apply_vs_text_edit(edit)
-	local bufnr = vim.api.nvim_get_current_buf()
-	local start_line = edit.range.start.line
-	local start_char = edit.range.start.character
-	local end_line = edit.range["end"].line
-	local end_char = edit.range["end"].character
-
-	local newText = string.gsub(edit.newText, "\r", "")
-	local lines = vim.split(newText, "\n")
-	local placeholder_row = -1
-	local placeholder_col = -1
-
-	for i, line in ipairs(lines) do
-		if line:match("^(///)") then
-			local indentation = string.rep(" ", start_char - 3)
-			lines[i] = indentation .. line
-		end
-	end
-
-	for i, line in ipairs(lines) do
-		local placeholder = string.find(line, "%$0")
-
-		if placeholder then
-			lines[i] = string.gsub(line, "%$0", "", 1)
-			placeholder_row = start_line + i - 1
-			placeholder_col = placeholder - 1
-
-			break
-		end
-	end
-
-	vim.api.nvim_buf_set_text(bufnr, start_line, start_char, end_line, end_char, lines)
-
-	if placeholder_row ~= -1 and placeholder_col ~= -1 then
-		local win = vim.api.nvim_get_current_win()
-		vim.api.nvim_win_set_cursor(win, { placeholder_row + 1, placeholder_col })
-	end
-end
-
 vim.api.nvim_create_autocmd("InsertCharPre", {
 	pattern = "*.cs",
 	callback = function()
@@ -103,7 +64,45 @@ return {
 				return
 			end
 
-			apply_vs_text_edit(result._vs_textEdit)
+			local edit = result._vs_textEdit
+
+			local bufnr = vim.api.nvim_get_current_buf()
+			local start_line = edit.range.start.line
+			local start_char = edit.range.start.character
+			local end_line = edit.range["end"].line
+			local end_char = edit.range["end"].character
+
+			local newText = string.gsub(edit.newText, "\r", "")
+			local lines = vim.split(newText, "\n")
+
+			local placeholder_row = -1
+			local placeholder_col = -1
+
+			for i, line in ipairs(lines) do
+				if line:match("^(///)") then
+					local indentation = string.rep(" ", start_char - 3)
+					lines[i] = indentation .. line
+				end
+			end
+
+			for i, line in ipairs(lines) do
+				local placeholder = string.find(line, "%$0")
+
+				if placeholder then
+					lines[i] = string.gsub(line, "%$0", "", 1)
+					placeholder_row = start_line + i - 1
+					placeholder_col = placeholder - 1
+
+					break
+				end
+			end
+
+			vim.api.nvim_buf_set_text(bufnr, start_line, start_char, end_line, end_char, lines)
+
+			if placeholder_row ~= -1 and placeholder_col ~= -1 then
+				local win = vim.api.nvim_get_current_win()
+				vim.api.nvim_win_set_cursor(win, { placeholder_row + 1, placeholder_col })
+			end
 		end,
 	},
 }
