b5ebd4bacc777205fac5b7d3d70c67d4557803f2
CPea <cpea2506@gmail.com>
2025-06-25 21:29:17 +0700

    Add `crates.nvim` back

--- lua/pea/plugins/lsp.lua
+++ lua/pea/plugins/lsp.lua
@@ -45,4 +45,29 @@ return {
             filewatching = "roslyn",
         },
     },
+    {
+        "saecki/crates.nvim",
+        event = "BufRead Cargo.toml",
+        opts = {
+            date_format = "%d-%m-%Y",
+            disable_invalid_feature_diagnostic = true,
+            popup = {
+                autofocus = true,
+                border = "rounded",
+            },
+            lsp = {
+                enabled = true,
+                actions = true,
+                completion = true,
+                hover = true,
+            },
+            completion = {
+                crates = {
+                    enabled = true,
+                    max_results = 8,
+                    min_chars = 3,
+                },
+            },
+        },
+    },
 }
