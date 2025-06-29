e95a1c44e0c633280f236ee8a47a24f33d9cf167
CPea <cpea2506@gmail.com>
2025-06-29 23:24:47 +0700

    Use `init` for `vim.g.*` config

--- lua/pea/plugins/lazygit.lua
+++ lua/pea/plugins/lazygit.lua
@@ -3,7 +3,7 @@ return {
     keys = {
         { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
-    config = function()
+    init = function()
         vim.g.lazygit_floating_window_winblend = 0
         vim.g.lazygit_floating_window_scaling_factor = 1
     end,
