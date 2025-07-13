2482c82a8bb4a05fb49d93fd49c7d2b7550d8fb7
CPea <cpea2506@gmail.com>
2025-07-13 15:28:46 +0700

    Remove `arena.nvim`

--- lua/pea/plugins/buffers.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "dzfrias/arena.nvim",
-    keys = {
-        { "<leader>sb", "<cmd>ArenaToggle<cr>", desc = "Toggle Arena" },
-    },
-    opts = {
-        ignore_current = false,
-    },
-}
