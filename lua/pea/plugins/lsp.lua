9e11a0a4536ce17c4cb5546f39a96507a4dc3a03
CPea <cpea2506@gmail.com>
2025-10-07 10:18:00 +0700

    Add `roslyn-filewatch` plugin

--- lua/pea/plugins/lsp.lua
+++ lua/pea/plugins/lsp.lua
@@ -43,6 +43,11 @@ return {
         },
     },
     {
+        "khoido2003/roslyn-filewatch.nvim",
+        ft = "cs",
+        opts = {},
+    },
+    {
         "saecki/crates.nvim",
         event = "BufRead Cargo.toml",
         opts = {
