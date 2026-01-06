f8fce5f13cf147e0c08f0e1c0f7f6f8e62be3aa5
CPea <cpea2506@gmail.com>
2026-01-06 12:52:01 +0700

    Open explorer on unamed buffer

--- lua/pea/plugins/explorer.lua
+++ lua/pea/plugins/explorer.lua
@@ -4,6 +4,29 @@ return {
     keys = {
         { "<leader>e", "<cmd>Fyler<cr>", desc = "Open Explorer" },
     },
+    init = function()
+        vim.api.nvim_create_autocmd("BufEnter", {
+            group = vim.api.nvim_create_augroup("pea_explorer", { clear = true }),
+            callback = function(args)
+                vim.schedule(function()
+                    local file = args.file
+                    local buf_name = vim.api.nvim_buf_get_name(0)
+                    local is_no_name_buffer = buf_name == "" and vim.bo.filetype == "" and vim.bo.buftype == ""
+                    local is_directory = vim.fn.isdirectory(file) == 1
+
+                    if not is_no_name_buffer and not is_directory then
+                        return
+                    end
+
+                    if is_directory then
+                        vim.cmd.cd(file)
+                    end
+
+                    require("fyler").open { kind = "replace" }
+                end)
+            end,
+        })
+    end,
     opts = function()
         local icons = require "pea.ui.icons"
 
