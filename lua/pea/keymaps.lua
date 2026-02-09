f08adf9de2a67b0583bde5817ece63e4ab89975a
CPea <cpea2506@gmail.com>
2026-02-09 20:25:00 +0700

    Remove description from keymaps settings

--- lua/pea/keymaps.lua
+++ lua/pea/keymaps.lua
@@ -1,21 +1,18 @@
 local keymaps = {
-    { "n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" } },
-    { "n", "<C-s>", "<cmd>w<cr>", { desc = "Save File" } },
-    { "n", "<C-e>", "<cmd>BufClose<cr>", { desc = "Close Buffer" } },
+    { "n", "<leader>q", "<cmd>q<cr>" },
+    { "n", "<C-s>", "<cmd>w<cr>" },
+    { "n", "<C-e>", "<cmd>BufClose<cr>" },
 
-    -- Better window movement.
-    { "n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true } },
-    { "n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true } },
-    { "n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true } },
-    { "n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true } },
+    { "n", "<C-h>", "<C-w>h", { remap = true } },
+    { "n", "<C-j>", "<C-w>j", { remap = true } },
+    { "n", "<C-k>", "<C-w>k", { remap = true } },
+    { "n", "<C-l>", "<C-w>l", { remap = true } },
 
-    -- Better indenting.
     { "v", "<", "<gv" },
     { "v", ">", ">gv" },
 
-    -- Lazy specific
-    { "n", "<leader>ph", "<cmd>Lazy<cr>", { desc = "Lazy Status" } },
-    { "n", "<leader>ps", "<cmd>Lazy sync<cr>", { desc = "Lazy Sync" } },
+    { "n", "<leader>ph", "<cmd>Lazy<cr>" },
+    { "n", "<leader>ps", "<cmd>Lazy sync<cr>" },
 }
 
 for _, key in pairs(keymaps) do
